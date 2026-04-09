2e6e920bbce7cf57c12248e1b3f25fc14943a03c
Neil Chatterjee <neilchatterjee2021@gmail.com>
2026-04-09 12:16:53 -0400

    Added Code, Problem Almost Complete

--- Speeding Ticket/Speeding_ticket.py
+++ Speeding Ticket/Speeding_ticket.py
@@ -7,4 +7,13 @@ for i in range(len(readfile)):
     #print(readfile[i])
 for i in range(len(readfile)):
     for j in range(0,2):
-        print(readfile[i][j])
+        readfile[i][j] = int(readfile[i][j])
+        #print(readfile[i][j])
+n = readfile[0][0]
+m = readfile[0][1]
+for i in range(1, 100):
+    if i <= readfile[1][0]:
+        if readfile[1][1] < readfile[4][1]:
+            speed_disparity1 = readfile[4][1] - readfile[1][1]
+print(speed_disparity1)
+
